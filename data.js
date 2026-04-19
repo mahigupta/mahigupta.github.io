0b6285f63b447ffa93529c8685423990f734eb8e
mahigupta <mahesh.gupta.nitb@gmail.com>
2026-04-19 15:35:30 -0700

    Fmaily tree project under my github site

---
+++ data.js
@@ -0,0 +1,530 @@
+var familyData = [
+    {
+        id: '1',
+        data: { 
+            first_name: 'Smt Ram Devi', 
+            last_name: 'Gupta', 
+            nickname: 'Mamma', 
+            birthday: '', 
+            avatar: 'images/mummy.jpeg', 
+            bio: '', 
+            occupation: 'Homemaker', 
+            education: '', 
+            gender: 'F', 
+            location: { lat: 23.521969489272106, lng: 87.31579737545253, label: 'Durgapur, West Bengal' } 
+        },
+        rels: { spouses: ['2'], children: ['3', '4', '5', '6', '7', '8', '9', '10'] }
+    },
+    {
+        id: '2',
+        data: { 
+            first_name: 'Late Shri Siya Sharan', 
+            last_name: 'Gupta', 
+            nickname: 'Papa', 
+            birthday: '', 
+            avatar: 'images/papa.jpeg', 
+            bio: '', 
+            occupation: 'Ayurvedic Doctor', 
+            education: '', 
+            gender: 'M',
+            deceased: true,
+        },
+        rels: { spouses: ['1'], children: ['3', '4', '5', '6', '7', '8', '9', '10'] }
+    },
+    {
+        id: '3',
+        data: {    
+            first_name: 'Sushma', 
+            last_name: 'Gupta', 
+            nickname: 'Guddan', 
+            birthday: '5th September', 
+            avatar: 'images/sushma.jpeg', 
+            bio: '', 
+            occupation: 'Teacher', 
+            education: '', 
+            gender: 'F',
+            location: { lat: 25.888681623911594, lng: 78.33799780291153, label: 'Dabra, Madhya Pradesh' }  
+        },
+        rels: { spouses: ['11'], children: ['19', '22'], parents: ['1', '2'] }
+    },
+    {
+        id: '4',
+        data: { 
+            first_name: 'Rupa', 
+            last_name: 'Gupta', 
+            nickname: 'Rupa', 
+            birthday: '13th December', 
+            avatar: 'images/rupa.jpeg', 
+            bio: '', 
+            occupation: 'Homemaker', 
+            education: 'MA Economics', 
+            gender: 'F',
+            location: { lat: 22.080488903710563, lng: 82.1422025991808, label: 'Bilaspur, Chhattisgarh' }  
+        },
+        rels: { spouses: ['12'], children: ['23', '24'], parents: ['1', '2'] }
+    },
+    {
+        id: '5',
+        data: { 
+            first_name: 'Dr Sanjay', 
+            last_name: 'Gupta', 
+            nickname: 'Sanjay', 
+            birthday: '29th June', 
+            avatar: 'images/sanjay.jpg', 
+            bio: '', 
+            occupation: 'Dean, Medical School', 
+            education: 'MBBS, MD', 
+            gender: 'M',
+            location: { lat: 23.270595658654354, lng: 77.41481441949732, label: 'Bhopal, Madhya Pradesh' }  
+        },
+        rels: { spouses: ['13'], children: ['25', '26'], parents: ['1', '2'] }
+    },
+    {
+        id: '6',
+        data: { 
+            first_name: 'Manoj', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '11th December', 
+            avatar: 'images/manoj.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BE (Electronics), MTech', 
+            gender: 'M',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' }  
+        },
+        rels: { spouses: ['14'], children: ['27', '28'], parents: ['1', '2'] }
+    },
+    {
+        id: '7',
+        data: { 
+            first_name: 'Mamta', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '20th August', 
+            avatar: 'images/mamta.jpeg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BE (CSE)', 
+            gender: 'F',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' }
+        },
+        rels: { spouses: ['15'], children: ['29'], parents: ['1', '2'] }
+    },
+    {
+        id: '8',
+        data: { 
+            first_name: 'Shrikant', 
+            last_name: 'Gupta', 
+            nickname: 'Rinku', 
+            birthday: '12th December', 
+            avatar: 'images/rinku.jpeg', 
+            bio: '', 
+            occupation: 'DGM, NSPCL India', 
+            education: 'BE (Electronics)', 
+            gender: 'M',
+            location: { lat: 23.521969489272106, lng: 87.31579737545253, label: 'Durgapur, West Bengal' }  
+        },
+        rels: { spouses: ['16'], children: ['30', '31'], parents: ['1', '2'] }
+    },
+    {
+        id: '9',
+        data: { 
+            first_name: 'Mahesh', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '4th August', 
+            avatar: 'images/mahesh.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BTech(IT), MS (CS)', 
+            gender: 'M',
+            location: { lat: 37.33536104851249, lng: -121.8597298496179, label: 'San Jose, California' } 
+        },
+        rels: { spouses: ['17'], children: ['32'], parents: ['1', '2'] }
+    },
+    {
+        id: '10',
+        data: { 
+            first_name: 'Ruchi', 
+            last_name: 'Gupta', 
+            nickname: 'Puchi', 
+            birthday: '22nd November', 
+            avatar: 'images/ruchi.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BE(CSE)', 
+            gender: 'F',
+            location: { lat: 17.41948789529526, lng: 78.49660674677266, label: 'Hyderabad, Telangana' }  
+        },
+        rels: { spouses: ['18'], children: ['33'], parents: ['1', '2'] }
+    },
+    {
+        id: '11',
+        data: { 
+            first_name: 'Vinod', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '15th July', 
+            avatar: 'images/vinod.png', 
+            bio: '', 
+            occupation: 'Businessman', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 25.888681623911594, lng: 78.33799780291153, label: 'Dabra, Madhya Pradesh' } 
+        },
+        rels: { spouses: ['3'], children: ['19', '22'] }
+    },
+    {
+        id: '12',
+        data: { 
+            first_name: 'Satish', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '1st October', 
+            avatar: 'images/satish.jpg', 
+            bio: '', 
+            occupation: 'Sr Manager, Coal India', 
+            education: 'AMIE (Mechanical Engineering)', 
+            gender: 'M',
+            location: { lat: 22.080488903710563, lng: 82.1422025991808, label: 'Bilaspur, Chhattisgarh' }
+        },
+        rels: { spouses: ['4'], children: ['23', '24'] }
+    },
+    {
+        id: '13',
+        data: { 
+            first_name: 'Rakhi', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '11th October', 
+            avatar: 'images/rakhi.jpg', 
+            bio: '', 
+            occupation: 'Homemaker, Social Worker', 
+            education: 'BSc, MBA', 
+            gender: 'F',
+            location: { lat: 23.270595658654354, lng: 77.41481441949732, label: 'Bhopal, Madhya Pradesh' } 
+        },
+        rels: { spouses: ['5'], children: ['25', '26'] }
+    },
+    {
+        id: '14',
+        data: { 
+            first_name: 'Pooja', 
+            last_name: 'Gupta', 
+            nickname: 'Honey', 
+            birthday: '3rd November', 
+            avatar: 'images/pooja.jpg', 
+            bio: '', 
+            occupation: 'Homemaker', 
+            education: 'BSc, C-DAC', 
+            gender: 'F',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' } 
+        },
+        rels: { spouses: ['6'], children: ['27', '28'] }
+    },
+    {
+        id: '15',
+        data: { 
+            first_name: 'Ankush', 
+            last_name: 'Garg', 
+            nickname: '', 
+            birthday: '19th December', 
+            avatar: 'images/ankush.jpg', 
+            bio: '', 
+            occupation: 'Director, Consulting, Deloitte India', 
+            education: 'BE, MBA', 
+            gender: 'M',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' } 
+        },
+        rels: { spouses: ['7'], children: ['29'] }
+    },
+    {
+        id: '16',
+        data: { 
+            first_name: 'Vidhi', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '11th April', 
+            avatar: 'images/vidhi.jpeg', 
+            bio: '', 
+            occupation: 'Homemaker', 
+            education: 'BSc, MCA', 
+            gender: 'F',
+            location: { lat: 23.521969489272106, lng: 87.31579737545253, label: 'Durgapur, West Bengal' } 
+        },
+        rels: { spouses: ['8'], children: ['30', '31'] }
+    },
+    {
+        id: '17',
+        data: { 
+            first_name: 'Rupal', 
+            last_name: 'Gupta', 
+            nickname: 'Anu', 
+            birthday: '10th October', 
+            avatar: 'images/rupal.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BE(CSE)', 
+            gender: 'F',
+            location: { lat: 37.33536104851249, lng: -121.8597298496179, label: 'San Jose, California' } 
+        },
+        rels: { spouses: ['9'], children: ['32'] }
+    },
+    {
+        id: '18',
+        data: { 
+            first_name: 'Varun', 
+            last_name: 'Prakash', 
+            nickname: '', 
+            birthday: '26th June', 
+            avatar: 'images/varun.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BE, MTech', 
+            gender: 'M',
+            location: { lat: 17.41948789529526, lng: 78.49660674677266, label: 'Hyderabad, Telangana' }
+        },
+        rels: { spouses: ['10'], children: ['33'] }
+    },
+    {
+        id: '19',
+        data: { 
+            first_name: 'Shikha', 
+            last_name: 'Gupta', 
+            nickname: 'Rani', 
+            birthday: '29th October', 
+            avatar: 'images/shikhu.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BSc, MCA', 
+            gender: 'F',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' }
+        },
+        rels: { spouses: ['20'], children: ['21'], parents: ['3', '11'] }
+    },
+    {
+        id: '20',
+        data: { 
+            first_name: 'Saurabh', 
+            last_name: 'Johri', 
+            nickname: '', 
+            birthday: '29th August', 
+            avatar: 'images/saurabh.jpeg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'BSc, MCA', 
+            gender: 'M',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' } 
+        },
+        rels: { spouses: ['19'], children: ['21'] }
+    },
+    {
+        id: '21',
+        data: { 
+            first_name: 'Sudhiksha', 
+            last_name: 'Johri', 
+            nickname: 'Sudhi', 
+            birthday: '23rd December', 
+            avatar: 'images/sudhi.jpeg', 
+            bio: '', 
+            occupation: 'Boss Baby', 
+            education: '', 
+            gender: 'F',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' } 
+        },
+        rels: { parents: ['19', '20'] }
+    },
+    {
+        id: '22',
+        data: { 
+            first_name: 'Late Shubham', 
+            last_name: 'Gupta', 
+            nickname: 'Sunny', 
+            birthday: '3rd October', 
+            avatar: 'images/shubham.jpg', 
+            bio: '', 
+            occupation: 'Sales and Marketing, OLX India', 
+            education: 'BSc, MBA', 
+            gender: 'M',
+            deceased: true, 
+        },
+        rels: { parents: ['3', '11'] }
+    },
+    {
+        id: '23',
+        data: { 
+            first_name: 'Adarsh', 
+            last_name: 'Gupta', 
+            nickname: 'Prince', 
+            birthday: '29th June', 
+            avatar: 'images/adarsh.jpg', 
+            bio: '', 
+            occupation: 'Software Engineer', 
+            education: 'B Tech', 
+            gender: 'M',
+            location: { lat: 17.41948789529526, lng: 78.49660674677266, label: 'Hyderabad, Telangana' }
+        },
+        rels: { parents: ['4', '12'] }
+    },
+    {
+        id: '24',
+        data: { 
+            first_name: 'Dr Akansha', 
+            last_name: 'Gupta', 
+            nickname: 'Akku', 
+            birthday: '8th December', 
+            avatar: 'images/akku.jpg', 
+            bio: '', 
+            occupation: 'Medical Student', 
+            education: 'MBBS', 
+            gender: 'F',
+            location: { lat: 21.253566502049278, lng: 81.63108567665014, label: 'Raipur, Chhattisgarh' } 
+        },
+        rels: { parents: ['4', '12'] }
+    },
+    {
+        id: '25',
+        data: { 
+            first_name: 'Dr Eshita', 
+            last_name: 'Gupta', 
+            nickname: 'Gunn', 
+            birthday: '4th September', 
+            avatar: 'images/gunn.jpg', 
+            bio: '', 
+            occupation: 'Medical Student', 
+            education: 'MBBS', 
+            gender: 'F',
+            location: { lat: 18.52928071245661, lng: 73.8787397952776, label: 'Pune, Maharashtra' } 
+        },
+        rels: { parents: ['13', '5'] }
+    },
+    {
+        id: '26',
+        data: { 
+            first_name: 'Eshan', 
+            last_name: 'Gupta', 
+            nickname: 'Kishna', 
+            birthday: '10th November', 
+            avatar: 'images/eshan.jpg', 
+            bio: 'I wanna play cricket for India.', 
+            occupation: 'Student', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 23.270595658654354, lng: 77.41481441949732, label: 'Bhopal, Madhya Pradesh' }  
+        },
+        rels: { parents: ['13', '5'] }
+    },
+    {
+        id: '27',
+        data: { 
+            first_name: 'Aryan', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '22nd September', 
+            avatar: 'images/aryan.png', 
+            bio: '', 
+            occupation: 'Engineering Student', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 15.460601365979242, lng: 75.01259933339826, label: 'Dharwad, Karnataka' }
+        },
+        rels: { parents: ['14', '6'] }
+    },
+    {
+        id: '28',
+        data: { 
+            first_name: 'Aarav', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '16th July', 
+            avatar: 'images/aarav.jpg', 
+            bio: '', 
+            occupation: 'Student', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' }
+        },
+        rels: { parents: ['14', '6'] }
+    },
+    {
+        id: '29',
+        data: { 
+            first_name: 'Ansh', 
+            last_name: 'Garg', 
+            nickname: '', 
+            birthday: '3rd May', 
+            avatar: 'images/ansh.jpg', 
+            bio: '', 
+            occupation: 'Student', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 12.973350843644926, lng: 77.59315049123066, label: 'Bangalore, Karnataka' } 
+        },
+        rels: { parents: ['7', '15'] }
+    },
+    {
+        id: '30',
+        data: { 
+            first_name: 'Sanvi', 
+            last_name: 'Gupta', 
+            nickname: 'Kuhu', 
+            birthday: '11th October', 
+            avatar: 'images/kuhu.jpeg', 
+            bio: '', 
+            occupation: 'Student', 
+            education: '', 
+            gender: 'F',
+            location: { lat: 23.521969489272106, lng: 87.31579737545253, label: 'Durgapur, West Bengal' } 
+        },
+        rels: { parents: ['16', '8'] }
+    },
+    {
+        id: '31',
+        data: { 
+            first_name: 'Samarth', 
+            last_name: 'Gupta', 
+            nickname: '', 
+            birthday: '23rd August', 
+            avatar: 'images/samarth.jpeg', 
+            bio: 'Samarth Loves Cars', 
+            occupation: 'Student', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 23.521969489272106, lng: 87.31579737545253, label: 'Durgapur, West Bengal' } 
+        },
+        rels: { parents: ['16', '8'] }
+    },
+    {
+        id: '32',
+        data: { 
+            first_name: 'Vedant', 
+            last_name: 'Gupta', 
+            nickname: 'Vedu', 
+            birthday: '9th July', 
+            avatar: 'images/vedant.jpg', 
+            bio: 'Vedant loves cars.', 
+            occupation: 'Boss Baby', 
+            education: '', 
+            gender: 'M',
+            location: { lat: 37.33536104851249, lng: -121.8597298496179, label: 'San Jose, California' } 
+        },
+        rels: { parents: ['17', '9'] }
+    },
+    {
+        id: '33',
+        data: { 
+            first_name: 'Coming', 
+            last_name: 'Soon', 
+            nickname: '', 
+            birthday: '', 
+            avatar: '', 
+            bio: '', 
+            occupation: 'Boss Baby', 
+            education: '', 
+            gender: '?',
+            location: { lat: 17.41948789529526, lng: 78.49660674677266, label: 'Hyderabad, Telangana' }
+        },
+        rels: { parents: ['10', '18'] }
+    }
+];
